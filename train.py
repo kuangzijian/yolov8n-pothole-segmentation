0bc838fbe8c5cb54f96ed1da0c5f3f3e26b89c33
Zijian Kuang <kuangzijian1@hotmail.com>
2023-05-29 21:08:42 -0600

    updated train file

--- train.py
+++ train.py
@@ -10,31 +10,13 @@ from ultralyticsplus import YOLO, render_result
 if __name__ == '__main__':
 
     # load model
-    model = YOLO('keremberke/yolov8n-pothole-segmentation')
+    model = YOLO("yolov8n-seg.pt")
 
-    # set model parameters
-    model.overrides['conf'] = 0.25  # NMS confidence threshold
-    model.overrides['iou'] = 0.45  # NMS IoU threshold
-    model.overrides['agnostic_nms'] = False  # NMS class-agnostic
-    model.overrides['max_det'] = 1000  # maximum number of detections per image
-
-    # load images
-    filePath = 'dataset/test'
-
-    # Get a list of all files in the directory
-    file_list = glob.glob(os.path.join(filePath, '*'))
-
-    # Loop through each file
-    for image in file_list:
-        file_name = os.path.basename(image)
-        print("Predict result for image: " + file_name)
-
-        # perform inference
-        results = model.predict(image)
-
-        # observe results
-        print(results[0].boxes)
-        print(results[0].masks)
-        render = render_result(model=model, image=image, result=results[0])
-        render.save('dataset/inference/' + file_name, 'png')
-        #render.show()
+    # start training
+    results = model.train(
+        batch=8,
+        device="cpu",
+        data="data.yaml",
+        epochs=7,
+        imgsz=120,
+    )
